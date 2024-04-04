ee4070eec2cc00cdf3473d1c8fd568917f56792b
JoseFausti <joseemilianofausti@gmail.com>
2024-04-04 12:10:08 -0300

    new

--- src/Ejercicios_Clase_4/Ejercicio_3/Interfaces/Personaje.java
+++ src/Ejercicios_Clase_4/Ejercicio_3/Interfaces/Personaje.java
@@ -3,5 +3,5 @@ package Ejercicios_Clase_4.Ejercicio_3.Interfaces;
 public interface Personaje {
     int atacar();
     int defender(int danio);
-    void recibirDanio(int danio);
+    void recibirDanio(int danio); // Codigo Personalizado
 }
