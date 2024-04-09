96cb378bda0ac5ab1e355694df7264e99f2dcfe9
JoseFausti <joseemilianofausti@gmail.com>
2024-04-09 18:42:55 -0300

    new

---
+++ src/Trabajos_Practicos/Ejercicio_10/Tipo.java
@@ -0,0 +1,5 @@
+package Trabajos_Practicos.Ejercicio_10;
+
+public enum Tipo {
+    Historica, Romantica, Policiaca, Realista, CienciaFiccion,Aventuras;
+}
---
+++ src/Trabajos_Practicos/Ejercicio_9/FiguraGeometrica.java
@@ -0,0 +1,8 @@
+package Trabajos_Practicos.Ejercicio_9;
+
+public abstract class FiguraGeometrica {
+
+    //Metodos
+    public abstract double calcularPerimetro();
+    public abstract double calcularArea();
+}
