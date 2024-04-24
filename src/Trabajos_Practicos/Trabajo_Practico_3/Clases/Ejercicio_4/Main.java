d28174b8e21d7480ee97081511c4cd98ceed06e1
JoseFausti <joseemilianofausti@gmail.com>
2024-04-23 21:03:31 -0300

    new

--- src/Trabajos_Practicos/Trabajo_Practico_3/Clases/Ejercicio_4/Main.java
+++ src/Trabajos_Practicos/Trabajo_Practico_3/Clases/Ejercicio_4/Main.java
@@ -6,16 +6,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Student> students = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-
+        Student student = new Student(null, 0, 0);
         while (true) {
             System.out.println("\nMenú:");
             System.out.println("1. Agregar nuevo alumno");
-            System.out.println("2. Mostrar lista de students");
+            System.out.println("2. Mostrar lista de estudiantes");
             System.out.println("3. Calcular y mostrar la media de notas");
-            System.out.println("4. Mostrar el alumno con la nota más alta");
-            System.out.println("5. Buscar alumno por nombre");
+            System.out.println("4. Mostrar al estudiante con la nota más alta");
+            System.out.println("5. Buscar estudiante por nombre");
             System.out.println("6. Salir");
             System.out.print("Elija una opción: ");
 
@@ -24,32 +23,19 @@ public class Main {
 
             switch (option) {
                 case 1:
-                    try {
-                        System.out.print("Nombre del alumno: ");
-                        String name = scanner.nextLine();
-                        System.out.print("Edad del alumno: ");
-                        int age = scanner.nextInt();
-                        System.out.print("Nota del alumno: ");
-                        double grade = scanner.nextDouble();
-
-                        Student newStudent = new Student(name, age, grade);
-                        students.put(name, newStudent);
-                        System.out.println("Alumno agregado con éxito.");
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
+                    student.addStudent();
                     break;
                 case 2:
-                    showStudentList(students);
+                    Student.showStudentList();
                     break;
                 case 3:
-                    calculateMeanGrades(students);
+                    Student.calculateMeanGrades();
                     break;
                 case 4:
-                    showStudentBestGrade(students);
+                    Student.showStudentBestGrade();
                     break;
                 case 5:
-                    searchStudentByName(students, scanner);
+                    Student.searchStudentByName();
                     break;
                 case 6:
                     System.out.println("Saliendo del programa...");
@@ -57,68 +43,6 @@ public class Main {
                 default:
                     System.out.println("Opción no válida. Intente nuevamente.");
             }
-        }
-    }
-
-    private static void showStudentList(Map<String, Student> students) {
-        if (students.isEmpty()) {
-            System.out.println("La lista de alumnos está vacía.");
-            return;
-        }
-
-        System.out.println("\nLista de Alumnos:");
-        for (Student student : students.values()) {
-            System.out.println(student);
-        }
-    }
-
-    private static void calculateMeanGrades(Map<String, Student> students) {
-        if (students.isEmpty()) {
-            System.out.println("No hay alumnos para calcular la media de las notas.");
-            return;
-        }
-
-        double sumGrades = 0;
-        for (Student student : students.values()) {
-            sumGrades += student.getGrade();
-        }
-
-        double mean = sumGrades / students.size();
-        System.out.println("La media de notas de todos los alumnos es: " + mean);
-    }
-
-    private static void showStudentBestGrade(Map<String, Student> students) {
-        if (students.isEmpty()) {
-            System.out.println("No hay alumnos para mostrar.");
-            return;
-        }
-
-        Student studentBestGrade = null;
-        for (Student student : students.values()) {
-            if (studentBestGrade == null || student.getGrade() > studentBestGrade.getGrade()) {
-                studentBestGrade = student;
-            }
-        }
-
-        System.out.println("Alumno con la nota más alta:");
-        System.out.println(studentBestGrade);
-    }
-
-    private static void searchStudentByName(Map<String, Student> students, Scanner scanner) {
-        if (students.isEmpty()) {
-            System.out.println("No hay alumnos para buscar.");
-            return;
-        }
-
-        System.out.print("Ingrese el nombre del alumno a buscar: ");
-        String searchedName = scanner.nextLine();
-
-        Student student = students.get(searchedName);
-        if (student != null) {
-            System.out.println("Información del alumno encontrado:");
-            System.out.println(student);
-        } else {
-            System.out.println("No se encontró ningún alumno con ese nombre.");
         }
     }
 }
--- src/Trabajos_Practicos/Trabajo_Practico_3/Clases/Ejercicio_4/Student.java
+++ src/Trabajos_Practicos/Trabajo_Practico_3/Clases/Ejercicio_4/Student.java
@@ -1,9 +1,16 @@
 package Trabajos_Practicos.Trabajo_Practico_3.Clases.Ejercicio_4;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Student {
+
     private String name;
     private int age;
     private double grade;
+    private static Map<String, Student> students = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
 
     public Student(String name, int age, double grade) {
         this.name = name;
@@ -30,5 +37,80 @@ public class Student {
                 ", Edad= " + age +
                 ", Nota= " + grade +
                 '}';
+    }
+
+    public void addStudent() {
+        System.out.print("Nombre del alumno: ");
+        String name = scanner.nextLine();
+        System.out.print("Edad del alumno: ");
+        int age = scanner.nextInt();
+        System.out.print("Nota del alumno: ");
+        double grade = scanner.nextDouble();
+
+        Student newStudent = new Student(name, age, grade);
+        students.put(name, newStudent);
+        System.out.println("Alumno agregado con éxito.");
+    }
+
+    public static void showStudentList() {
+        if (students.isEmpty()) {
+            System.out.println("La lista de alumnos está vacía.");
+            return;
+        }
+
+        System.out.println("\nLista de Alumnos:");
+        for (Student student : students.values()) {
+            System.out.println(student);
+        }
+    }
+
+    public static void calculateMeanGrades() {
+        if (students.isEmpty()) {
+            System.out.println("No hay alumnos para calcular la media de las notas.");
+            return;
+        }
+
+        double sumGrades = 0;
+        for (Student student : students.values()) {
+            sumGrades += student.getGrade();
+        }
+
+        double mean = sumGrades / students.size();
+        System.out.println("La media de notas de todos los alumnos es: " + mean);
+    }
+
+    public static void showStudentBestGrade() {
+        if (students.isEmpty()) {
+            System.out.println("No hay alumnos para mostrar.");
+            return;
+        }
+
+        Student studentBestGrade = null;
+        for (Student student : students.values()) {
+            if (studentBestGrade == null || student.getGrade() > studentBestGrade.getGrade()) {
+                studentBestGrade = student;
+            }
+        }
+
+        System.out.println("Alumno con la nota más alta:");
+        System.out.println(studentBestGrade);
+    }
+
+    public static void searchStudentByName() {
+        if (students.isEmpty()) {
+            System.out.println("No hay alumnos para buscar.");
+            return;
+        }
+
+        System.out.print("Ingrese el nombre del alumno a buscar: ");
+        String searchedName = scanner.nextLine();
+
+        Student student = students.get(searchedName);
+        if (student != null) {
+            System.out.println("Información del alumno encontrado:");
+            System.out.println(student);
+        } else {
+            System.out.println("No se encontró ningún alumno con ese nombre.");
+        }
     }
 }
